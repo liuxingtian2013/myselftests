a1e5011d1da834eaa423d4d3b2bb4e5f9b0eafab
liuxingtian2013 <1175417076@qq.com>
2018-05-03 10:53:00 +0800

    擦肩

--- app/src/main/java/com/shanxixinhe/system/service/MainActivity.java
+++ app/src/main/java/com/shanxixinhe/system/service/MainActivity.java
@@ -2,7 +2,10 @@ package com.shanxixinhe.system.service;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/*
+* 提交测试
+*
+* */
 public class MainActivity extends AppCompatActivity {
 
     @Override
